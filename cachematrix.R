ce6285cfb544f09a6615cae90a920a24fd1b4036
Doug Seven <doug@dougseven.com>
2020-10-09 18:34:25 -0700

    Added two functions for assignment, plus one test function for validating the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and 
+## the matrix has not changed), then the cachesolve should retrieve the 
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
+
+## This function is for testing. To test, load the functions into your
+## environment, and execute the following commands. 
+
+## a <- NULL
+## b <- NULL
+## createTestMatrix(a,b) 
+## x <- makeCacheMatrix(a)
+## cacheSolve(x)
+## identical(cacheSolve(x), b)    
+## The result will be:
+## getting cached data
+## [1] TRUE
+
+createTestMatrix <- function(a, b) {
+  a1 <- c(3,2,5)
+  a2 <- c(2,3,2)
+  a3 <- c(5,2,4)
+  
+  a <<- rbind(a1,a2,a3)
+  b <<- solve(A)
 }
